f46aa78d0e88f727af14e8be01aa50ee6ca01298
castronox <castronox@outlook.es>
2022-04-14 13:01:43 +0200

    Prueba de comando para hacer commit y añadir al seguimiento + mensaje.

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,7 @@ public class Main {
 
         System.out.println(minombre1);
         System.out.println("esto es una prueba de commit");
+        System.out.println ("3er commit de prueba");
+
     }
 }
