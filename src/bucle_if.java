e203eebe310de93ae9e6c77560f40eea83d4f3d3
castronox <castronox@outlook.es>
2022-04-23 13:26:58 +0200

    iniciamos ejercicio del bucle if invocando java util scanner para entradas en consola, asignando un variable vacia.

---
+++ src/bucle_if.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class bucle_if {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner( System.in);
+        System.out.println ( "Introduce un numero Positivo o negativo:");
+
+        int nota = sc.nextInt ();
+}
+    }
