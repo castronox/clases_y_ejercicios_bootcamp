fb513e589696bc685b45ba94860d8486f63190bd
castronox <castronox@outlook.es>
2022-07-07 12:45:30 +0200

    Ejercicios del tema 4

---
+++ src/switches.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.lang.*;
+
+public class switches{
+
+public static void main(String[] args) {
+     int numero1 = 1;
+     int numero2 = 2;
+     int numero3 = 3;
+     int numero4 = 4;
+    Scanner sc = new Scanner( System.in);
+    System.out.println ("Introduce una ESTACIÓN del año:");
+    System.out.println("pulsando el 1 se activa el VERANO");
+    System.out.println("pulsando el 2 se activa la PRIMAVERA");
+    System.out.println("pulsando el 3 se activa el INVIERNO");
+    System.out.println("pulsando el 4 se activa el OTOÑO");
+    
+    int estacion = sc.nextInt ();
+    
+    int numeroestacion = estacion ;
+
+    
+
+    switch (numeroestacion){
+
+        case 1:
+             System.out.println("La estacion es Verano");
+             break;
+
+        case 2:
+             System.out.println("La estacion es Primavera");
+             break;
+        
+        case 3:
+             System.out.println("La estacion es Invierno");
+             break;
+
+        case 4:
+             System.out.println("La estacion es Otoño");
+             break;
+
+        default : 
+            System.out.println("Este es el default, hasta pronto!");
+    }
+
+}
+
+
+}
