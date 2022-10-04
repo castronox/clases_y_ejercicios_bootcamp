006425742b176be406c0cd306fceda8723dfc44e
castronox <castronox@outlook.es>
2022-10-04 09:35:58 +0200

    Creación de contrusctor con y si parametros.

--- src/pOrientadaObjetos.java
+++ src/pOrientadaObjetos.java
@@ -5,21 +5,30 @@ public class pOrientadaObjetos {
         Coche coche = new Coche();
 
         System.out.println("Coche1:");
+
+        System.out.println("velocidad actual: ");
         System.out.println(coche.velocidadActual);
 
-        coche.acelerar();
+        System.out.println("velocidad Maxima: ");
+        System.out.println(coche.velocidadMaxima);
 
-        System.out.println(coche.velocidadActual);
+        System.out.println("numero de puertas: ");
+        System.out.println(coche.numeroDePuertas);
 
-        coche.decelerar();
 
-        System.out.println(coche.velocidadActual);
-
-        Coche coche2 = new Coche();
+        Coche coche2 = new Coche(5,120,80 );
 
         System.out.println("Coche 2:");
+
+
+        System.out.println("velocidad actual: ");
         System.out.println(coche2.velocidadActual);
+
+        System.out.println("velocidad maxima: ");
         System.out.println(coche2.velocidadMaxima);
+
+        System.out.println("numero de puertas: ");
+        System.out.println(coche2.numeroDePuertas);
 
     }
 }
@@ -34,11 +43,12 @@ class Coche{
     int velocidadMaxima;
     float velocidadActual;
 
-    public Coche(){
 
+
+    public Coche() {
         numeroDePuertas = 5;
         velocidadMaxima = 120;
-        System.out.println("Estoy en el constructor.");
+        System.out.println("Estoy en el constructor ***sin nada***");
     }
 
     public void acelerar() {
@@ -47,6 +57,17 @@ class Coche{
     public void decelerar() {
         velocidadActual -=3;
     }
+
+
+    public Coche(int puertas, int velocidadMax, int velocidadAct ){
+
+        numeroDePuertas = puertas;
+        velocidadMaxima = velocidadMax;
+        velocidadActual = velocidadAct;
+        System.out.println("Estoy en el constructor CON PARAMETROS.");
+    }
+
+
 
 
 }
