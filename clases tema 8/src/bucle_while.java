ab66326f1ed63eefdcb525eae39389b5900478b6
castronox <41049193+castronox@users.noreply.github.com>
2022-10-06 09:29:40 +0200

    Add files via upload

---
+++ clases tema 8/src/bucle_while.java
@@ -0,0 +1,14 @@
+public class bucle_while {
+    public static void main(String[] args) {
+
+
+       int numerowhile = 1;
+
+       while ( numerowhile <=3){
+
+           System.out.println("El numero es : " + numerowhile);
+
+           numerowhile ++;
+       }
+    }
+}
