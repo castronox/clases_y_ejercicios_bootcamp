48fe166e0c0c11cf26cf054f2c32de86ffe90257
castronox <castronox@outlook.es>
2022-10-06 10:30:31 +0200

    Creación de clase persona, sin gets ni sets.

--- ejercicios_tema_8/ejercicios_tema_8/src/Main.java
+++ ejercicios_tema_8/ejercicios_tema_8/src/Main.java
@@ -1,8 +1,33 @@
+
+
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("ab");
+        //Instanciamos la clase. --2
+
+        Persona persona = new Persona();
+
+
+        //invocamos las propiedades asignadas a una variable. --3
+        persona.nombre = "Andres";
+        persona.edad = 14;
+        persona.telefono = 556543534;
+
+
+        //imprimimos en pantalla concatenando la impresión en consola.
+        System.out.println("Nombre del usuario: " + persona.nombre);
+        System.out.println("Edad del usuario: " + persona.edad);
+        System.out.println("Telefono del usuario: " + persona.telefono);
 
 
     }
+}
+
+//Creamos la clase. --1
+class Persona {
+    String nombre;
+    int edad;
+    int telefono;
+
 }
