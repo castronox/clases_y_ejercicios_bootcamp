71264348b60236eed16db6554275bb5527ac7d92
castronox <castronox@outlook.es>
2022-10-06 10:46:14 +0200

    Creación de clase persona, construimos los getters.

--- ejercicios_tema_8/ejercicios_tema_8/src/Main.java
+++ ejercicios_tema_8/ejercicios_tema_8/src/Main.java
@@ -2,49 +2,45 @@
 
 
 public class Main {
-
     public static void main(String[] args) {
         //Instanciamos la clase. --2
-
         Persona persona = new Persona();
-
-
         //invocamos las propiedades asignadas a una variable. --3
         persona.nombre = "Andres";
         persona.edad = 14;
         persona.telefono = 556543534;
 
-
         //imprimimos en pantalla concatenando la impresión en consola. --4
         System.out.println("Nombre del usuario: " + persona.nombre);
         System.out.println("Edad del usuario: " + persona.edad);
         System.out.println("Telefono del usuario: " + persona.telefono);
-
-
     }
 }
-
 //Creamos la clase. --1
 class Persona {
-
 
     //Construimos los setters. --5
     private String nombre;
     public void setNombre(String nombre){
         this.nombre = nombre;
     }
-
     private int edad;
     public void setEdad(int edad){
         this.edad = edad;
     }
-
-
     private int telefono;
     public void setTelefono(int telefono){
         this.telefono = telefono;
     }
 
-
-
+    //Construimos los getters
+    public String getNombre(){
+        return this.nombre;
+    }
+    public int getEdad(){
+        return  this.edad;
+    }
+    public int getTelefono(){
+        return this.telefono;
+    }
 }
