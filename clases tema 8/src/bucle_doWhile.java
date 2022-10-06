c26d1a6868925f2ad8daca8fb17af78035fcbe93
castronox <castronox@outlook.es>
2022-10-06 09:34:24 +0200

    nuevo repositorio organizado

---
+++ clases tema 8/src/bucle_doWhile.java
@@ -0,0 +1,17 @@
+public class bucle_doWhile
+{
+
+    public static void main(String[] args)
+    {
+        int numero_doWhile = 3;
+
+        do {
+            System.out.println("el valor del numero es:" + numero_doWhile) ;
+
+            numero_doWhile ++;
+        }
+
+        while (numero_doWhile < 3);
+    }
+}
+
