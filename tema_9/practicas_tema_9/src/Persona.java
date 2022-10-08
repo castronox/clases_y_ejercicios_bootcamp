6525d0cc01b2779b85945d3d414d71505042fcae
castronox <castronox@outlook.es>
2022-10-09 01:07:16 +0200

    comienzo de declaraciones de las propiedades del objeto.

---
+++ tema_9/practicas_tema_9/src/Persona.java
@@ -0,0 +1,12 @@
+public class Persona {
+    private int edad;
+    private float altura;
+    private String nombre;
+    private String correo;
+
+    public String getNombre(){return nombre;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+
+
+
+}
