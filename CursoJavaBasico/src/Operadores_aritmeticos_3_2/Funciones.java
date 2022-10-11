ccda63ff94dc9c26158ba5c73c9775ced0e6c80f
castronox <castronox@outlook.es>
2022-10-11 10:22:11 +0200

    Funciones

--- CursoJavaBasico/src/Operadores_aritmeticos_3_2/Funciones.java
+++ CursoJavaBasico/src/Operadores_aritmeticos_3_2/Funciones.java
@@ -3,7 +3,24 @@ package Operadores_aritmeticos_3_2;
 public class Funciones {
 
     public static void main(String[] args) {
+
+        muestraMenu();
+
+
+
         // opcion 1:
+
+        }
+
+    static void muestraMenu(){
+        System.out.println("Bienvenidos a la tienda de cristina!");
+        System.out.println("1-prendas");
+        System.out.println("2-chaquetas");
+        System.out.println("3-bolsos");
+
+
+
+
 
 
     }
