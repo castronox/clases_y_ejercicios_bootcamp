b32140511efbafcb7c108b0a6dfa3cdc64e9dee9
castronox <castronox@outlook.es>
2022-10-11 09:57:44 +0200

    Aprendiendo a debuguear.
    Al lado del boton run.
    y seleccionar los puntos de las lineas de codigo a observar

--- CursoJavaBasico/src/Operadores_aritmeticos_3_2/OperadoresCopmaracion.java
+++ CursoJavaBasico/src/Operadores_aritmeticos_3_2/OperadoresCopmaracion.java
@@ -1,5 +1,25 @@
 package Operadores_aritmeticos_3_2;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class OperadoresCopmaracion {
+
+    /*   Compaación
+        < Menor que
+        > Mayor que
+        <=Menor igual que
+        >=Mayor igual que
+        == igual que  */
+    public static void main(String[] args) {
+        int numero1 = 32;
+        int numero2 = 89;
+        int resultado = numero1*numero2;
+        boolean resultadobol = resultado>numero2;
+
+        System.out.println(resultadobol);
+    }
+
+
+
 
 }
