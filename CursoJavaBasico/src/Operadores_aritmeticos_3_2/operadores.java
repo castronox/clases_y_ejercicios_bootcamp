10f6a9cf92279ab7d84b4e259f9061e276f480ff
castronox <castronox@outlook.es>
2022-10-11 09:29:56 +0200

    declaración de operadores aritmeticos.
    con codigo reutilizable.
    Evitar hacer opreaciones repetidamente, no repetir codigo

--- CursoJavaBasico/src/Operadores_aritmeticos_3_2/operadores.java
+++ CursoJavaBasico/src/Operadores_aritmeticos_3_2/operadores.java
@@ -7,8 +7,9 @@ public class operadores {
         //aritméticos:
         int numero1 = 10;
         int numero2 = 20;
+        int resultado = numero1 + numero2;
 
-        System.out.println(numero1+numero2);
+        System.out.println(resultado);
     }
 
 
