41435c73df3ac5474e7d73f4af858910f4544c31
castronox <castronox@outlook.es>
2022-10-11 10:04:26 +0200

    Clase Operadores Lógicos.
    descripción de los opradores.

--- CursoJavaBasico/src/Operadores_aritmeticos_3_2/OperadoresLogicos.java
+++ CursoJavaBasico/src/Operadores_aritmeticos_3_2/OperadoresLogicos.java
@@ -3,6 +3,9 @@ package Operadores_aritmeticos_3_2;
 public class OperadoresLogicos {
 
     public static void main(String[] args) {
+        /*Operadores Lógicos
+        *
+        * AND -  && - OR - ||   */
 
         System.out.println();
     }
