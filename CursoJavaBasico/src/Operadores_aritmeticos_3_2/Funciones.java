9fc3783365d370cd242c38aa7546c7b9a1f5830b
castronox <castronox@outlook.es>
2022-10-13 11:47:43 +0200

    Funciones metodo 2.

--- CursoJavaBasico/src/Operadores_aritmeticos_3_2/Funciones.java
+++ CursoJavaBasico/src/Operadores_aritmeticos_3_2/Funciones.java
@@ -3,16 +3,27 @@ package Operadores_aritmeticos_3_2;
 public class Funciones {
 
     public static void main(String[] args) {
-        muestraMenu();
-        }
 
         // opcion 1
+        /* muestraMenu();
+        }  */
+
+
+        //Opcion 2 --> Fucnción sin parámetros y con tipo de retorno.
+
     static void muestraMenu(){
+
+        // opcion 1
 
         System.out.println("Bienvenidos a la tienda de cristina!");
         System.out.println("1-prendas");
         System.out.println("2-chaquetas");
         System.out.println("3-bolsos");
+    }
+       //Opcion 2 Aplicar gets.
+    static String getMuestraMenu(){
+        return "Bienvenidos a la tienda de cristina xD! \n 1- Ver Prendas \n 2- Chaquetas \n 3-Bolsos";
+
     }
 
 
