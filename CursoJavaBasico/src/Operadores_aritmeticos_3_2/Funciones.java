4ba400dedca3b964e6ab9522595395f803948311
castronox <castronox@outlook.es>
2022-10-13 12:03:04 +0200

    Funciones metodo 2. (Parte 3)CORRECIÓN DE ERRORES DE NOVATO "lLAVE POR AHI PERDIDA QUE NO HACIA NADA.".

--- CursoJavaBasico/src/Operadores_aritmeticos_3_2/Funciones.java
+++ CursoJavaBasico/src/Operadores_aritmeticos_3_2/Funciones.java
@@ -5,27 +5,32 @@ public class Funciones {
     public static void main(String[] args) {
 
         // opcion 1
-        /* muestraMenu();
-        }  */
-
-
+        muestraMenu();
         //Opcion 2 --> Fucnción sin parámetros y con tipo de retorno.
         String menu = getMenu();
+        System.out.println(menu);
+    }
+
+
+    //Opcion 2 --> Fucnción sin parámetros y con tipo de retorno.
+
+
+
 
     static void muestraMenu(){
 
         // opcion 1
 
-        System.out.println("Bienvenidos a la tienda de cristina!");
+        System.out.println("Bienvenidos a la tienda de cristina! --> METODO 1 <--");
         System.out.println("1-prendas");
         System.out.println("2-chaquetas");
         System.out.println("3-bolsos");
     }
 
 
-}
+
     //Opcion 2 Aplicar gets.
     static String getMenu() {
-        return "Bienvenidos al e-commerce de cristina ferre atelier: \n Prendas \n Chaquetas \n Bolsos";
+        return "Bienvenidos al e-commerce de cristina ferre atelier --> METODO 2 <-- : \n Prendas \n Chaquetas \n Bolsos";
     }
-    }
+}
