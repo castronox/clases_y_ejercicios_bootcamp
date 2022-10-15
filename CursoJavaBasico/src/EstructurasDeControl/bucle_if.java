ba9f20b7f6f394e0f8fb7b3bb19466cf02fda548
castronox <castronox@outlook.es>
2022-10-15 20:42:07 +0200

    Estructuras de control bucle If else.

--- CursoJavaBasico/src/EstructurasDeControl/bucle_if.java
+++ CursoJavaBasico/src/EstructurasDeControl/bucle_if.java
@@ -1,6 +1,15 @@
 package EstructurasDeControl;
+import java.util.Scanner;
 
 public class bucle_if {
-
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce una edad.");
+        int edad = sc.nextInt();
+        if (edad >= 18) {
+            System.out.println("Es mayor de edad.");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+    }
 }
