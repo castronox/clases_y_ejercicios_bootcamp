258ff72b7d8b7e8310078f4ce160b8bc2390e335
castronox <castronox@outlook.es>
2022-10-15 19:59:17 +0200

    Funciones metodo 4. Funcion con parametros y tipo de retorno.

---
+++ CursoJavaBasico/src/Operadores_aritmeticos_3_2/Funciones2.java
@@ -0,0 +1,20 @@
+package Operadores_aritmeticos_3_2;
+
+public class Funciones2 {
+
+    public static void main(String[] args) {
+       int numero1 = 198;
+       int numero2 = 456;
+
+        System.out.println("El metodo nº 4 de funciones daría como resultado : "+ sumar(numero1,numero2));
+
+    }
+
+    static int sumar(int n1, int n2) {
+        int num1 = n1;
+        int num2 = n2;
+
+        return num1 + num2;
+
+    }
+}
