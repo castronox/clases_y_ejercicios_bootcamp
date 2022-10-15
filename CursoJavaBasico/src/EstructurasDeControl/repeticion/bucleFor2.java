e91cd74d45ac55fb2ba5405ce9bdccfabeb37bed
castronox <castronox@outlook.es>
2022-10-15 22:26:40 +0200

    Construccion de bucle for metodo 2 con arrays..

---
+++ CursoJavaBasico/src/EstructurasDeControl/repeticion/bucleFor2.java
@@ -0,0 +1,12 @@
+package EstructurasDeControl.repeticion;
+
+public class bucleFor2 {
+    public static void main(String[] args) {
+
+        String[] nombres = {"Pepe","Armando","Maradona","Diego"};
+        for(int i = 0 ; i < nombres.length; i++){
+
+            System.out.println("Nombre "+ i +" "+ nombres[i]);
+        }
+    }
+}
