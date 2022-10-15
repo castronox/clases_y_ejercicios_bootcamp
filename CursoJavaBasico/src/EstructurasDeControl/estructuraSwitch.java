59a0d5632c3c48d353885ebb06ab6f9553d03563
castronox <castronox@outlook.es>
2022-10-15 21:51:38 +0200

    Declaramos las sentencias de control switch.

--- CursoJavaBasico/src/EstructurasDeControl/estructuraSwitch.java
+++ CursoJavaBasico/src/EstructurasDeControl/estructuraSwitch.java
@@ -1,4 +1,45 @@
 package EstructurasDeControl;
 
 public class estructuraSwitch {
+    public static void main(String[] args) {
+        String dia = "Lunes";
+
+        switch (dia){
+            case "Lunes":
+                System.out.println("Estamos a Lunes");
+                break;
+        }
+        switch (dia){
+            case "Martes":
+                System.out.println("Estamos a Martes");
+                break;
+        }
+        switch (dia){
+            case "Miercoles":
+                System.out.println("Estamos a Miercoles");
+                break;
+        }
+        switch (dia){
+            case "Jueves":
+                System.out.println("Estamos a Jueves");
+                break;
+        }
+        switch (dia){
+            case "Viernes":
+                System.out.println("Estamos a Viernes");
+                break;
+        }
+        switch (dia){
+            case "Sabado":
+                System.out.println("Estamos a Sabado");
+                break;
+        }
+        switch (dia){
+            case "Domingo":
+                System.out.println("Estamos a Domingo");
+                break;
+        }
+
+
+    }
 }
