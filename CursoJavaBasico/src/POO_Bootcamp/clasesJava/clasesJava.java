06dad9db77a522c2954d936c911fe2a434a2cc11
castronox <castronox@outlook.es>
2022-10-18 19:43:02 +0200

    Clases desde la 5.0 hasta 5.4 "Métodos".

---
+++ CursoJavaBasico/src/POO_Bootcamp/clasesJava/clasesJava.java
@@ -0,0 +1,35 @@
+package POO_Bootcamp.clasesJava;
+
+public class clasesJava {
+
+    //Atributos.
+
+    String fabricante;
+    String modelo;
+    Double cubicaje;
+    int year;
+    boolean sport;
+    boolean traccion4;
+
+
+    //Constructores.
+
+    public vehiculo(){
+    }
+
+    //Sobrecarga
+    public Vehiculo(String fabricante,String modelo,Double cubicaje,int year, boolean sport, boolean traccion4){
+
+        this.fabricante=fabricante;
+        this.modelo=modelo;
+        this.cubicaje=cubicaje;
+        this.year=year;
+        this.sport=sport;
+        this.traccion4=traccion4;
+
+
+     // Métodos.
+
+
+    }
+}
